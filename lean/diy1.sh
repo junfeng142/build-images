24387e97a455fd8543d29b2ec3035086c0bce560
junfeng142 <35558806+junfeng142@users.noreply.github.com>
2022-09-24 21:29:03 +0800

    Add files via upload

---
+++ lean/diy1.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# clone own
+git clone https://github.com/junfeng142/packages.git package/own
+
+# clone passwall
+#git clone -b packages https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+#git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/passwall/luci
+
+# clone helloworld
+git clone https://github.com/fw876/helloworld.git package/helloworld
+
+# clone bypass
+#git clone https://github.com/kiddin9/openwrt-bypass.git package/bypass
+
+# clone clash
+#git clone -b test https://github.com/frainzy1477/luci-app-clash.git package/own/luci-app-clash
+
+# clone aliyun
+git clone https://github.com/messense/aliyundrive-webdav.git package/aliyunwebd
+git clone https://github.com/messense/aliyundrive-fuse.git package/aliyunfuse
+
+# Uncomment password
+sed -i '/shadow/s/^/#/g' package/lean/default-settings/files/zzz-default-settings
+
+# clone luci-app-amlogic
+#git clone https://github.com/ophub/luci-app-amlogic.git package/own/luci-app-amlogic
+
+# clone N1HK1dabao
+git clone https://github.com/Netflixxp/N1HK1dabao.git N1HK1dabao
