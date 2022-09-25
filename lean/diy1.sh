52c0b3111104583d87e9df769404ec6464530059
junfeng142 <35558806+junfeng142@users.noreply.github.com>
2022-09-25 19:11:46 +0800

    Update diy1.sh

--- lean/diy1.sh
+++ lean/diy1.sh
@@ -38,3 +38,6 @@ sed -i '/shadow/s/^/#/g' package/lean/default-settings/files/zzz-default-setting
 
 # clone N1HK1dabao
 git clone https://github.com/Netflixxp/N1HK1dabao.git N1HK1dabao
+
+# add kernel4.4
+patch -p1 <  package/own/patches/add_kernel.patch
