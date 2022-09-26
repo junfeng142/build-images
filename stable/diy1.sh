16decdcce4df9fadefd2ccb751a24411d35f7d6b
junfeng142 <35558806+junfeng142@users.noreply.github.com>
2022-09-26 21:08:59 +0800

    Update diy1.sh

--- stable/diy1.sh
+++ stable/diy1.sh
@@ -39,3 +39,6 @@ patch -p1 < package/own/patches/add-sfe-flowoffload-for-stable.patch
 patch -p1 < package/own/patches/add-patch_dts_file-wifi-xradio.patch
 patch -p1 < package/own/patches/add-patch_sun8i-h3-ths.patch
 patch -p1 < package/own/patches/add-patch_sun8i-spi0flash_16M-usb2-usb3-uart1-uart2.patch
+
+# add kernel4.4 for arm32
+patch -p1 < package/own/patches/add_kernel4.4_for_arm32.patch
