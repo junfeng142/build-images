ee64c9f7f76cad48483c4154445b620a67c86910
junfeng142 <35558806+junfeng142@users.noreply.github.com>
2022-09-27 11:48:03 +0800

    Update diy1.sh

--- stable/diy1.sh
+++ stable/diy1.sh
@@ -36,6 +36,7 @@ git clone https://github.com/messense/aliyundrive-fuse.git package/aliyunfuse
 patch -p1 < package/own/patches/add-sfe-flowoffload-for-stable.patch
 
 # add cputemp/wifi/usb for orangepizero
+patch -p1 < package/own/patches/fit-for-mac80211.patch
 patch -p1 < package/own/patches/add-patch_dts_file-wifi-xradio.patch
 patch -p1 < package/own/patches/add-patch_sun8i-h3-ths.patch
 patch -p1 < package/own/patches/add-patch_sun8i-spi0flash_16M-usb2-usb3-uart1-uart2.patch
