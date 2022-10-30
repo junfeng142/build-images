acae56ea7ad3d7bde353e8ae1bc3f604a89021a6
junfeng142 <35558806+junfeng142@users.noreply.github.com>
2022-10-30 15:50:04 +0800

    Update diy2.sh

--- immortalwrt/diy2.sh
+++ immortalwrt/diy2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.0.8/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.3/g' package/base-files/files/bin/config_generate
 
 # Modify
 sed -i 's/\"services\"/\"nas\"/g' package/aliyunwebd/openwrt/luci-app-aliyundrive-webdav/luasrc/controller/aliyundrive-webdav.lua
