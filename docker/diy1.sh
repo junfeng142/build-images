d472e1dc58edf3bd1e11972fc3a4c080f4478e89
junfeng142 <35558806+junfeng142@users.noreply.github.com>
2022-10-31 22:41:52 +0800

    Update diy1.sh

--- docker/diy1.sh
+++ docker/diy1.sh
@@ -16,14 +16,13 @@ git clone https://github.com/junfeng142/packages.git package/own
 # clone ssr
 #git clone -b ssr https://github.com/junfeng142/packages.git package/ssr
 
+# clone bypass
+git clone https://github.com/kiddin9/openwrt-bypass.git package/bypass
+patch -p1 < package/own/patches/add-Hans-for-bypass.patch
+
 # clone passwall
 #git clone -b packages https://github.com/xiaorouji/openwrt-passwall.git package/passwall
 #git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/passwall/luci
-
-# clone helloworld
-git clone https://github.com/fw876/helloworld.git package/helloworld
-patch -p1 < package/own/patches/add-packages-for-helloworld.patch
-sed -i '/PACKAGE_libustream/d' package/helloworld/luci-app-ssr-plus/Makefile
 
 # clone aliyun
 git clone https://github.com/messense/aliyundrive-webdav.git package/aliyunwebd
