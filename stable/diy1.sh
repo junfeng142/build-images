fea07d58bb69b4d899485a2b9bdc889d8879ab8e
junfeng142 <35558806+junfeng142@users.noreply.github.com>
2022-11-03 21:39:10 +0800

    Update diy1.sh

--- stable/diy1.sh
+++ stable/diy1.sh
@@ -24,6 +24,11 @@ git clone https://github.com/junfeng142/packages.git package/own
 git clone https://github.com/fw876/helloworld.git package/helloworld
 patch -p1 < package/own/patches/add-packages-for-helloworld.patch
 
+# clone bypass
+#git clone https://github.com/kiddin9/openwrt-bypass.git package/bypass
+#patch -p1 < package/own/patches/add-packages-for-bypass.patch
+#patch -p1 < package/own/patches/add-Hans-for-bypass.patch
+
 # clone aliyun
 git clone https://github.com/messense/aliyundrive-webdav.git package/aliyunwebd
 git clone https://github.com/messense/aliyundrive-fuse.git package/aliyunfuse
