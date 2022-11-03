b1414a643cc41072480eb1781069f725cf56ae5d
junfeng142 <35558806+junfeng142@users.noreply.github.com>
2022-11-03 21:38:25 +0800

    Update diy1.sh

--- docker/diy1.sh
+++ docker/diy1.sh
@@ -14,12 +14,7 @@
 git clone https://github.com/junfeng142/packages.git package/own
 
 # clone ssr
-#git clone -b ssr https://github.com/junfeng142/packages.git package/ssr
-
-# clone bypass
-git clone https://github.com/kiddin9/openwrt-bypass.git package/bypass
-patch -p1 < package/own/patches/add-packages-for-bypass.patch
-patch -p1 < package/own/patches/add-Hans-for-bypass.patch
+git clone -b ssr https://github.com/junfeng142/packages.git package/ssr
 
 # clone aliyun
 git clone https://github.com/messense/aliyundrive-webdav.git package/aliyunwebd
