7e4fa6c1bd799651444ae8339bf00e18ae3c84da
junfeng142 <35558806+junfeng142@users.noreply.github.com>
2022-11-25 20:57:13 +0800

    Update diy1.sh

--- docker/diy1.sh
+++ docker/diy1.sh
@@ -30,7 +30,7 @@ git clone https://github.com/frainzy1477/luci-app-clash.git package/my/luci-app-
 
 # clone bypass
 #git clone https://github.com/kiddin9/openwrt-bypass.git package/bypass
-#patch -p1 < package/own/patches/add-packages-for-bypass.patch
+#patch -p1 < package/own/patches/add-depends-for-bypass.patch
 #patch -p1 < package/own/patches/add-Hans-for-bypass.patch
 
 # clone aliyun
