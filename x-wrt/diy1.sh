3f22b3f920f1a9158a16527c63c52cf50b27c09a
junfeng142 <hero_cao@163.com>
2023-11-12 13:13:58 +0800

    x-wrt

--- x-wrt/diy1.sh
+++ x-wrt/diy1.sh
@@ -16,9 +16,8 @@ git clone https://github.com/junfeng142/packages.git package/own
 # clone bypass
 git clone -b bypass https://github.com/junfeng142/packages.git package/bypass
 
-# clone aliyun
-git clone https://github.com/messense/aliyundrive-webdav.git package/aliyunwebd
-git clone https://github.com/messense/aliyundrive-fuse.git package/aliyunfuse
+# clone helloworld
+git clone -b main https://github.com/fw876/helloworld.git package/helloworld
 
 # add usb_gadget kernel5.4
 cat package/own/configs/sunxi-config >> target/linux/sunxi/config-5.4
@@ -29,5 +28,3 @@ sed -i 's/rootwait/rootwait g_ether.dev_addr=f8:dc:7a:5e:32:02 g_ether.host_addr
 # Uncomment password
 sed -i '/shadow/s/^/#/g' package/emortal/default-settings/files/99-default-settings
 
-# add usbpower for hc5761
-patch -p1 < package/own/patches/fit_usb_dts_for_hc5761.patch
