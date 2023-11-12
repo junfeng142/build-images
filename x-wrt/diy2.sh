9c4f92af30b5d1c0d658fea5e8154e785d33370f
junfeng142 <35558806+junfeng142@users.noreply.github.com>
2023-11-12 14:04:47 +0800

    Update diy2.sh

--- x-wrt/diy2.sh
+++ x-wrt/diy2.sh
@@ -14,7 +14,7 @@
 sed -i 's/192.168.1.1/192.168.0.39/g' package/base-files/files/bin/config_generate
 
 # Modify
-sed -i 's/services/nas/g' feeds/luci/applications/luci-app-ksmbd/root/usr/share/luci/menu.d/luci-app-ksmbd.json
-sed -i 's/system/services/g' feeds/luci/applications/luci-app-cpufreq/root/usr/share/luci/menu.d/luci-app-cpufreq.json
-sed -i 's/Turbo ACC 网络加速/网络加速/g' feeds/luci/applications/luci-app-turboacc/po/zh_Hans/turboacc.po
+#sed -i 's/services/nas/g' feeds/luci/applications/luci-app-ksmbd/root/usr/share/luci/menu.d/luci-app-ksmbd.json
+#sed -i 's/system/services/g' feeds/luci/applications/luci-app-cpufreq/root/usr/share/luci/menu.d/luci-app-cpufreq.json
+#sed -i 's/Turbo ACC 网络加速/网络加速/g' feeds/luci/applications/luci-app-turboacc/po/zh_Hans/turboacc.po
 
