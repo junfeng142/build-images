a2a28600423e77396d99d18fd47400f462eb3297
junfeng142 <35558806+junfeng142@users.noreply.github.com>
2023-11-22 21:50:43 +0800

    Update diy2.sh

--- x-wrt/diy2.sh
+++ x-wrt/diy2.sh
@@ -25,3 +25,6 @@ sed -i 's/192.168.1.1/192.168.0.6/g' package/base-files/files/bin/config_generat
 # Modify
 #sed -i 's/\"services\"/\"system\"/g' feeds/luci/applications/luci-app-ttyd/luasrc/controller/ttyd.lua
 #sed -i 's#("ttyd")#("ttyd"), 10#g' feeds/luci/applications/luci-app-ttyd/luasrc/controller/ttyd.lua
+#sed -i 's/services/nas/g' feeds/luci/applications/luci-app-ksmbd/root/usr/share/luci/menu.d/luci-app-ksmbd.json
+#sed -i 's/system/services/g' feeds/luci/applications/luci-app-cpufreq/root/usr/share/luci/menu.d/luci-app-cpufreq.json
+#sed -i 's/services/system/g' feeds/luci/applications/luci-app-ttyd/root/usr/share/luci/menu.d/luci-app-ttyd.json
