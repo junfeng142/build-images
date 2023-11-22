c329a4b105bac2fccd5791bb070da5567aabfdc0
junfeng142 <35558806+junfeng142@users.noreply.github.com>
2023-11-22 21:04:06 +0800

    Update diy1.sh

--- lean/diy1.sh
+++ lean/diy1.sh
@@ -14,20 +14,17 @@
 git clone https://github.com/junfeng142/packages.git package/own
 
 # clone passwall
-git clone -b packages https://github.com/xiaorouji/openwrt-passwall.git package/passwall
-git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/passwall/luci
+#git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall
+#git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall/luci
 
 # clone helloworld
-#git clone https://github.com/fw876/helloworld.git package/helloworld
+git clone -b main https://github.com/fw876/helloworld.git package/helloworld
 
 # Uncomment password
 sed -i '/shadow/s/^/#/g' package/lean/default-settings/files/zzz-default-settings
-
-# clone luci-app-amlogic
-git clone https://github.com/ophub/luci-app-amlogic.git package/own/luci-app-amlogic
 
 # clone N1HK1dabao
 git clone https://github.com/Netflixxp/N1HK1dabao.git N1HK1dabao
 
 # kernel version
-sed -i 's/6.1/5.4/g' target/linux/x86/Makefile
+sed -i 's/6.1/5.15/g' target/linux/x86/Makefile
