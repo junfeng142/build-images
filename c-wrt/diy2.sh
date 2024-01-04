7bd11a205eec7f623f39a1494b7926e2eeb29f1d
junfeng142 <35558806+junfeng142@users.noreply.github.com>
2024-01-04 20:15:34 +0800

    Update diy2.sh

--- c-wrt/diy2.sh
+++ c-wrt/diy2.sh
@@ -14,16 +14,16 @@
 sed -i 's/192.168.1.1/192.168.0.7/g' package/base-files/files/bin/config_generate
 
 # fix dhcp/kvr and cgi-io
-#patch -p1 < package/own/patches/add-dhcp-kvr-for-luci19.patch
-#patch -p1 < package/own/patches/add-support-for-lucihttp.patch
+patch -p1 < package/own/patches/add-dhcp-kvr-for-luci19.patch
+patch -p1 < package/own/patches/add-support-for-lucihttp.patch
 
 # add cpu_temp for luci19.07
-#sed -i 's/thermal\/thermal_zone0\/temp/hwmon\/hwmon1\/temp1_input/g' package/own/patches/add-cputemp_for_arm_luci19.patch
-#patch -p1 < package/own/patches/add-cputemp_for_arm_luci19.patch
+sed -i 's/thermal\/thermal_zone0\/temp/hwmon\/hwmon1\/temp1_input/g' package/own/patches/add-cputemp_for_arm_luci19.patch
+patch -p1 < package/own/patches/add-cputemp_for_arm_luci19.patch
 
 # Modify
-#sed -i 's/\"services\"/\"system\"/g' feeds/luci/applications/luci-app-ttyd/luasrc/controller/ttyd.lua
-#sed -i 's#("ttyd")#("ttyd"), 10#g' feeds/luci/applications/luci-app-ttyd/luasrc/controller/ttyd.lua
-sed -i 's/services/nas/g' feeds/luci/applications/luci-app-ksmbd/root/usr/share/luci/menu.d/luci-app-ksmbd.json
+sed -i 's/\"services\"/\"system\"/g' feeds/luci/applications/luci-app-ttyd/luasrc/controller/ttyd.lua
+sed -i 's#("ttyd")#("ttyd"), 10#g' feeds/luci/applications/luci-app-ttyd/luasrc/controller/ttyd.lua
+#sed -i 's/services/nas/g' feeds/luci/applications/luci-app-ksmbd/root/usr/share/luci/menu.d/luci-app-ksmbd.json
 #sed -i 's/system/services/g' feeds/luci/applications/luci-app-cpufreq/root/usr/share/luci/menu.d/luci-app-cpufreq.json
-sed -i 's/services/system/g' feeds/luci/applications/luci-app-ttyd/root/usr/share/luci/menu.d/luci-app-ttyd.json
+#sed -i 's/services/system/g' feeds/luci/applications/luci-app-ttyd/root/usr/share/luci/menu.d/luci-app-ttyd.json
