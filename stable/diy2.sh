6cc6f1105831e45a189c1051229d7d394039c8fb
junfeng142 <35558806+junfeng142@users.noreply.github.com>
2024-01-06 12:05:31 +0800

    Update diy2.sh

--- stable/diy2.sh
+++ stable/diy2.sh
@@ -11,22 +11,22 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.1.3/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.4/g' package/base-files/files/bin/config_generate
 
 # add upx
 mv ./upx ./staging_dir/host/bin && chmod +x ./staging_dir/host/bin/upx
 
 # fix dhcp/kvr and cgi-io
-#patch -p1 < package/own/patches/add-dhcp-kvr-for-luci19.patch
-#patch -p1 < package/own/patches/add-support-for-lucihttp.patch
+patch -p1 < package/own/patches/add-dhcp-kvr-for-luci19.patch
+patch -p1 < package/own/patches/add-support-for-lucihttp.patch
 
 # add cpu_temp for luci19.07
-#sed -i 's/thermal\/thermal_zone0\/temp/hwmon\/hwmon1\/temp1_input/g' package/own/patches/add-cputemp_for_arm_luci19.patch
-#patch -p1 < package/own/patches/add-cputemp_for_arm_luci19.patch
+sed -i 's/thermal\/thermal_zone0\/temp/hwmon\/hwmon1\/temp1_input/g' package/own/patches/add-cputemp_for_arm_luci19.patch
+patch -p1 < package/own/patches/add-cputemp_for_arm_luci19.patch
 
 # Modify
-#sed -i 's/\"services\"/\"system\"/g' feeds/luci/applications/luci-app-ttyd/luasrc/controller/ttyd.lua
-#sed -i 's#("ttyd")#("ttyd"), 10#g' feeds/luci/applications/luci-app-ttyd/luasrc/controller/ttyd.lua
+sed -i 's/\"services\"/\"system\"/g' feeds/luci/applications/luci-app-ttyd/luasrc/controller/ttyd.lua
+sed -i 's#("ttyd")#("ttyd"), 10#g' feeds/luci/applications/luci-app-ttyd/luasrc/controller/ttyd.lua
 
 # fit batman-adv for easymesh
 #rm feeds/routing/batman-adv/patches/0004-Revert-batman-adv-genetlink-make-policy-common-to-fa.patch
