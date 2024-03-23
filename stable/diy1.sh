6fc499fd437bba34cf8603e0e6770304142155cf
junfeng142 <35558806+junfeng142@users.noreply.github.com>
2024-03-23 13:46:32 +0800

    Update diy1.sh

--- stable/diy1.sh
+++ stable/diy1.sh
@@ -14,19 +14,19 @@
 git clone https://github.com/junfeng142/packages.git package/own
 
 # clone ssr
-git clone -b ssr https://github.com/junfeng142/packages.git package/ssr
+#git clone -b ssr https://github.com/junfeng142/packages.git package/ssr
 
 # clone bypass
-#git clone -b bypass https://github.com/junfeng142/packages.git package/bypass
+git clone -b bypass https://github.com/junfeng142/packages.git package/bypass
 
 # clone helloworld
-#git clone -b main https://github.com/fw876/helloworld.git package/helloworld
+git clone -b main https://github.com/fw876/helloworld.git package/helloworld
 
 # clone alist
-git clone https://github.com/sbwml/luci-app-alist.git package/alist
+#git clone https://github.com/sbwml/luci-app-alist.git package/alist
 
 # fit for bypass
-#mv package/bypass/luci-app-bypass package/bypass/smartdns package/bypass/lua-maxminddb package/bypass/trojan-plus package/my && rm -rf package/bypass
+mv package/bypass/luci-app-bypass package/bypass/smartdns package/bypass/lua-maxminddb package/bypass/trojan-plus package/my && rm -rf package/bypass
 
 # Modify image size
 sed -i 's/tplink-8mlzma/tplink-16mlzma/g' target/linux/ar71xx/image/tiny-tp-link.mk
