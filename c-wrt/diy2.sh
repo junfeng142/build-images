2425c308a49fabbc19ef14083cdfea3977b30166
junfeng142 <35558806+junfeng142@users.noreply.github.com>
2024-07-06 09:38:22 +0800

    Update diy2.sh

--- c-wrt/diy2.sh
+++ c-wrt/diy2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.3/g' package/base-files/files/bin/config_generate
 
 # fix ddns/firewall
 patch -p1 < package/own/patches/fit-for_ddns_firewall.patch
