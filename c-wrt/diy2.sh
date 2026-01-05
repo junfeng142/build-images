6416f00702c3fd154b2212d97ccf73472590969a
junfeng142 <hero_cao@163.com>
2026-01-05 12:36:00 +0800

    add clash

--- c-wrt/diy2.sh
+++ c-wrt/diy2.sh
@@ -24,7 +24,7 @@ rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.1.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.4/g' package/base-files/files/bin/config_generate
 
 # fix ddns/firewall
 patch -p1 < package/own/patches/fit-for_ddns_firewall.patch
